65b117675bbed39c5cb6d4696cc8ffef9e4d6beb
w-ejt <97180065+w-ejt@users.noreply.github.com>
2023-02-02 10:53:31 +0000

    sql implementation for bomfinder

--- src/syspro_connect/bomfinder.py
+++ src/syspro_connect/bomfinder.py
@@ -3,60 +3,47 @@ from functools import lru_cache
 from .syspro import Syspro
 
 
-FBM_QUERY = """SELECT ParentPart, Component as stock_code, QtyPer FROM BomStructure"""
-MERGE_QUERY = """SELECT StockCode as stock_code, Description, MaterialCost, LabourCost FROM InvMaster"""
+QUERY = """
+WITH cte AS (
+    SELECT Component,
+        ParentPart,
+        CAST(QtyPer AS float) as QtyPer
+    FROM BomStructure
+    WHERE ParentPart = ?
+    UNION ALL
+    SELECT t.Component,
+        t.ParentPart,
+        CAST(t.QtyPer AS float) * cte.QtyPer as QtyPer
+    FROM BomStructure t
+        JOIN cte ON t.ParentPart = cte.Component
+)
+SELECT Component,
+    [Description],
+    QtyPer,
+    InvMaster.MaterialCost,
+    InvMaster.LabourCost,
+    UnitCost,
+    DateLastPurchase
+FROM cte
+    LEFT JOIN InvMaster ON cte.Component = InvMaster.StockCode
+    LEFT JOIN InvWarehouse ON cte.Component = InvWarehouse.StockCode
+ORDER BY MaterialCost DESC;
+"""
 
 
-class BOMFinder:
-    """
-    Find all components in a BOM.
-    Loads the entire BOM structure into memory on instantiation.
-    """
-
-    def __init__(self, syspro: Syspro) -> None:
+class Bomfinder:
+    def __init__(self, syspro: Syspro):
         self.syspro = syspro
-        self.fbm = self.syspro.get_df(FBM_QUERY)
 
-    def find(self, code: str) -> pd.DataFrame:
-        """
-        Find all components in a BOM.
-        """
-        if code not in self.valid_codes:
-            return pd.DataFrame()
-
-        return self._find_children(code).pipe(self._merge_details)
-
-    @property
-    def valid_codes(self):
-        return set(self.fbm.ParentPart.unique())
-
-    @lru_cache
-    def _find_children(self, code: str) -> pd.DataFrame:
-        children = self.fbm[self.fbm["ParentPart"] == code]
-        for child in children.itertuples():
-            grand_children = self._find_children(child.stock_code).assign(
-                QtyPer=lambda x: x.QtyPer * child.QtyPer
-            )
-            children = pd.concat([children, grand_children])
-
-        return children
-
-    def _merge_details(self, df: pd.DataFrame) -> pd.DataFrame:
-        codes_in_bom = set(df.stock_code.unique())
-        # any code passed here is already valid
-        param_placeholders = ",".join(["?"] * len(codes_in_bom))
-        query_suffix = f" WHERE StockCode IN ({param_placeholders})"
-
-        df = df.merge(
-            self.syspro.get_df(
-                sql=MERGE_QUERY + query_suffix,
-                params=codes_in_bom,
-            ),
-            on="stock_code",
-        )
-        return df
+    def get_bom(self, bom_num: str) -> pd.DataFrame:
+        return self.syspro.get_df(QUERY, params=[bom_num])
 
 
 if __name__ == "__main__":
     s = Syspro()
-    b = BOMFinder(s)
+    b = Bomfinder(s)
+
+    print(b.get_bom("79806"))
+
+
+# TODO: remove this for the sql implementation
